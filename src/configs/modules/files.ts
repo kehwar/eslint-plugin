4d77b358c3497ddc1ed36d366ed96a65463e362e
Kehwar <erickkwr@gmail.com>
2023-07-12 00:37:02 -0500

    fix: no longer necessary for nuxt-remote-fn

--- src/configs/modules/files.ts
+++ src/configs/modules/files.ts
@@ -9,21 +9,4 @@ export const getFilesConfig = createEslintConfig({
         /** @see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/filename-case.md */
         "unicorn/filename-case": ["error", { case: "kebabCase" } ],
     },
-
-    /** Rule overrides */
-    overrides: [
-        /** Override for remoteFunctionFn server files */
-        {
-            files: ["*.server.ts"],
-            rules: {
-                /**
-                 * Use snakeCase for filenames since they will be used as variable names
-                 *
-                 * @see https://github.com/wobsoriano/nuxt-remote-fn/issues/5
-                 * @see https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/filename-case.md
-                 */
-                "unicorn/filename-case": ["error", { case: "snakeCase" } ],
-            },
-        },
-    ],
 });
