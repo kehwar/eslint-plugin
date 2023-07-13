b7ee2e95ec5d31aea6783d1011a09c0cf22f4668
Kehwar <erickkwr@gmail.com>
2023-07-12 23:27:05 -0500

    chore: add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    /** This is the root file */
+    root: true,
+
+    /** Extensions */
+    extends: ["plugin:@kehwar/nuxt"],
+};
